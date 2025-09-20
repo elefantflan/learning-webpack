a3a03a2c4cc7373b4b9a98e10a8d0cdf5cf9d6c3
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-20 14:42:03 -0700

    added dev server options into web config under output. what to serve? > dist && port 3000 is for front end && open: true allows us to npm run dev and open web automatically && hot is reloading

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,16 @@ module.exports = {
         path: path.resolve(__dirname,"dist"),
         filename: "[name][contenthash].js",
     },
-    watch:true,
+    devServer:{
+        static:{
+            directory:path.resolve('__dirname', 'dist')
+        },
+        port:3000,
+        open:true,
+        hot:true,
+        compress: true,
+        historyApiFallback:true,
+    },
     module:{
         rules:[
             {
