aa688d22ff0737679c5798e947f8f9c2f7805d85
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-20 13:53:07 -0700

    added module.exports and require(path) inside webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
-module.export = {
-    mode: 'development'
+const path = require("path")
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname,"dist"),
+        filename: "bundle.js",
+    }
 }
