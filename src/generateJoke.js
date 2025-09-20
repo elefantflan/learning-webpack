7983ac2a8cc325395fb98b2c079be76be2189eae
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-20 15:24:30 -0700

    moved axios.get outside of generateJoke function

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -6,9 +6,11 @@ function generateJoke(){
             Accept: 'application/json'
         }
     }
-    axios.get('https://icanhazdadjoke.com', config).then(res => {
-        document.getElementById('joke').innerHTML = res.data.joke
-    })
 }
+
+
+axios.get('https://icanhazdadjoke.com', config).then(res => {
+    document.getElementById('joke').innerHTML = res.data.joke
+})
 
 export default generateJoke
